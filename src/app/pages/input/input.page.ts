b5e38810aabb7636d9211343b6a55073fc61d650
Yuki <yuki.marchal1216@gmail.com>
2025-02-07 13:14:07 +0100

    working

--- src/app/pages/input/input.page.ts
+++ src/app/pages/input/input.page.ts
@@ -1,20 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent } from '@ionic/angular/standalone';
+import { ButtonModule } from 'primeng/button'
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel'
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { InputNumberModule } from 'primeng/inputnumber'
+import { DatePickerModule } from 'primeng/datepicker';
+import { SelectModule } from 'primeng/select';
+import { RepetitionEnum } from 'src/app/core/models/transaction.model';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.page.html',
   styleUrls: ['./input.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, CommonModule, FormsModule, ButtonModule, FloatLabelModule, IftaLabelModule, InputNumberModule, InputTextModule, DatePickerModule, SelectModule ]
 })
 export class InputPage implements OnInit {
+
+  max: number = Number.MAX_VALUE;
+  title!: string;
+  amount!: number
+  isPositive: boolean = false;
+  currentTime!: Date;
+  isRepeating: boolean = false;
+  repeats!: string[];
+  selectedRepeat!: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.currentTime = new Date();
+
+    this.repeats = [
+      "No Repeat", 
+      "Daily", 
+      "Weekly", 
+      "Monthly", 
+      "Yearly", 
+    ];
+
+    this.selectedRepeat = this.repeats[0];
   }
 
+  changePositiveNegative(): void {
+    this.isPositive = !this.isPositive
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Material from '@primeng/themes/material';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -12,5 +15,11 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+          preset: Material
+      }
+    })
   ],
 });
