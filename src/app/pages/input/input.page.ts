f77a322829d84bb95c6586a0f04fa12f036617f8
Yuki <yuki.marchal1216@gmail.com>
2025-02-09 18:55:06 +0100

    working on form

--- src/app/pages/input/input.page.ts
+++ src/app/pages/input/input.page.ts
@@ -1,23 +1,39 @@
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormControlName, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControlName,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { IonContent } from '@ionic/angular/standalone';
-import { ButtonModule } from 'primeng/button'
+import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
-import { FloatLabelModule } from 'primeng/floatlabel'
+import { FloatLabelModule } from 'primeng/floatlabel';
 import { IftaLabelModule } from 'primeng/iftalabel';
-import { InputNumberModule } from 'primeng/inputnumber'
+import { InputNumberModule } from 'primeng/inputnumber';
 import { DatePickerModule } from 'primeng/datepicker';
 import { SelectModule } from 'primeng/select';
 import { RepetitionEnum } from 'src/app/core/models/transaction.model';
 import { Category } from 'src/app/core/models/category.model';
-import { TransactionType } from 'src/app/core/models/transactionType.model';
+import {
+  TransactionType,
+  TransactionTypeCreateForm,
+  TransactionTypeUpdateForm,
+} from 'src/app/core/models/transactionType.model';
 import { TextareaModule } from 'primeng/textarea';
 import { TransactionValidator } from 'src/app/shared/validators/transaction.validator';
+import { TransactionTypeService } from 'src/app/features/services/transaction-type.service';
+import { BaseComponent } from 'src/app/shared/templates/components/base/base.component';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { CategoryService } from 'src/app/features/services/category.service';
+import { MessageModule } from 'primeng/message'
 
 export interface Repetition {
-  id: number,
-  name: string
+  id: number;
+  name: string;
 }
 
 @Component({
@@ -25,101 +41,143 @@ export interface Repetition {
   templateUrl: './input.page.html',
   styleUrls: ['./input.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, ButtonModule, FloatLabelModule, IftaLabelModule, InputNumberModule, InputTextModule, DatePickerModule, SelectModule, TextareaModule, ReactiveFormsModule ]
+  imports: [
+    IonContent,
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    FloatLabelModule,
+    IftaLabelModule,
+    InputNumberModule,
+    InputTextModule,
+    DatePickerModule,
+    SelectModule,
+    TextareaModule,
+    ReactiveFormsModule,
+    MessageModule
+  ],
 })
-
 export class InputPage implements OnInit {
-
   max: number = Number.MAX_VALUE;
   isPositive: boolean = false;
-  isOneTime: boolean = true;
   repetitions!: Repetition[];
   types!: TransactionType[];
   categories!: Category[];
 
-  form!: FormGroup
-  name!: string;
-  amount!: number
-  repetition!: Repetition;
-  setDate!: Date;
-  endDate?: Date;
-  transactionType!: TransactionType;
-  category?: Category;
-  note: string = "";
+  form!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private serviceTT: TransactionTypeService, private serviceC: CategoryService) { }
 
   ngOnInit() {
 
-    /********** Date **********/
+    /********** FormGroup setting **********/
 
-    this.setDate = new Date();
+    this.form = this.fb.group(
+      {
+        name: ['', [Validators.required, Validators.minLength(1)], []],
+        amount: [
+          null,
+          [Validators.required, Validators.min(0), Validators.max(this.max)],
+          [],
+        ],
+        repetition: [1, [Validators.required], []],
+        setDate: [new Date(), [Validators.required], []],
+        endDate: [null],
+        transactionTypeId: [
+          undefined,
+          [Validators.required, TransactionValidator.optionValid],
+          [],
+        ],
+        categoryId: [
+          undefined,
+          [Validators.required, TransactionValidator.optionValid],
+          [],
+        ],
+        note: ['', [], []],
+      },
+      { validators: TransactionValidator.endDateRequiredValidator }
+    );
 
-    /********** Repetition **********/
+    /********** Other default settings **********/
+
+    // endDate is diabled by default and when the repetition != no-repeat, it will be enabled
+    this.form.get('endDate')?.disable();
+    this.onChangeRepeat();
+
+    // get otions
+    this.getTypes();
+    this.getCategories();
     
     this.repetitions = [
-      { id: 1, name: "No Repeat" },
-      { id: 2, name: "Daily" },
-      { id: 3, name: "Weekly" },
-      { id: 4, name: "Monthly" },
-      { id: 5, name: "Yearly" },
-    ]
-    
-    this.repetition = this.repetitions[0];
+      { id: 1, name: 'No Repeat' },
+      { id: 2, name: 'Daily' },
+      { id: 3, name: 'Weekly' },
+      { id: 4, name: 'Monthly' },
+      { id: 5, name: 'Yearly' },
+    ];
+  }
 
-    /********** Transaction type **********/
-    
-    this.types = [
-      { id: 0, name: "No type selected"},
-      { id: 1, name: "One-time payment" },
-    ]
-    
-    this.transactionType = this.types[0];
+  /********** Methods **********/
 
-    /********** Category **********/
+  getTypes(): void {
+    this.serviceTT.get().subscribe(data => this.types = data);
+  }
 
-    this.categories = [
-      { id: 0, name: "No category selected"},
-      { id: 1, name: "Groceries" }
-    ]
-
-    this.category = this.categories[0];
-
-    /********** Form Group **********/
-
-    this.form = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(2)], []],
-      amount: [0, [Validators.required, Validators.min(0), Validators.max(this.max)], []],
-      repetition: [this.repetitions[0], [Validators.required], []],
-      setDate: [new Date, [Validators.required], []],
-      endDate: [null, [], []],
-      transactionType: [null, [Validators.required, TransactionValidator.optionValid], []],
-      category: [null, [Validators.required, TransactionValidator.optionValid], []],
-      note: ['', [], []],
-    }, {});
+  getCategories(): void {
+    this.serviceC.get().subscribe(data => this.categories = data);
   }
 
   changePositiveNegative(): void {
-    this.isPositive = !this.isPositive
+    this.isPositive = !this.isPositive;
   }
 
   onChangeRepeat() {
-    if (this.repetition == this.repetitions[0]){
-      this.isOneTime = true;
-    } else {
-      this.isOneTime = false;
-    }
+    this.form.get('repetition')?.valueChanges.subscribe(value => {
+      const endDateControl = this.form.get('endDate');
+
+      if (value == 1) {
+        this.form.get('endDate')?.disable();
+      } else {
+        this.form.get('endDate')?.enable();
+      }
+    });
   }
 
   send() {
+    this.form.markAllAsTouched();
+
+    if (this.form.valid) {
+      //TODO service.Create()
+      //TODO Amount needs to be controled with +/-
+    } else {
+      console.log('invalid form')
+    }
     console.log(this.form.valid);
     console.log(this.form.controls['name'].valid);
-    console.log(this.form.controls['name']);
+    console.log(this.form.controls['name'].value);
+    console.log(this.form.controls['amount'].valid);
+    console.log(this.form.controls['amount'].value);
+    console.log(this.form.controls['setDate'].valid);
+    console.log(this.form.controls['setDate'].value);
+    console.log(this.form.controls['repetition'].valid);
+    console.log(this.form.controls['repetition'].value);
+    console.log(this.form.controls['endDate'].valid);
+    console.log(this.form.controls['endDate'].value);
+    console.log(this.form.controls['transactionTypeId'].valid);
+    console.log(this.form.controls['transactionTypeId'].value);
+    console.log(this.form.controls['categoryId'].valid);
+    console.log(this.form.controls['categoryId'].value);
+    console.log(this.form.controls['note'].valid);
     console.log(this.form.controls['note'].value);
   }
 }
 
 /********** Note **********/
+
+//TODO todo list for HTML
+//TODO Change size of +/- icon
+//TODO Change Error message position for Amount
+//TODO Change SetDate and EndDate size
 
 // this.form = this.fb.group({
 //   name: [
--- src/app/shared/validators/transaction.validator.ts
+++ src/app/shared/validators/transaction.validator.ts
@@ -8,4 +8,12 @@ export class TransactionValidator {
     }
     return null;
   }
+
+  static endDateRequiredValidator(control: AbstractControl): ValidationErrors | null {
+    const repetition = control.get('repetition')?.value;
+    const endDate = control.get('endDate')?.value;
+
+    if (repetition == 1) return null;
+    else return {endDateRequired: true };
+  }
 }
