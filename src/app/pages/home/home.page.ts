c2ab99b54bf65008e31aa938f7125d8c0d8b3114
Yuki <yuki.marchal1216@gmail.com>
2025-02-14 18:39:37 +0100

    avalable and balance ok

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,8 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IonContent, IonButton } from '@ionic/angular/standalone';
+import dayjs from 'dayjs';
+import { AccountUpdateForm } from 'src/app/core/models/account.model';
 import { ChartComponent } from 'src/app/features/components/chart/chart.component';
 import { AccountService } from 'src/app/features/services/account.service';
+import { DetailService } from 'src/app/features/services/detail.service';
 import { UserService } from 'src/app/features/services/user.service';
 
 @Component({
@@ -11,42 +14,83 @@ import { UserService } from 'src/app/features/services/user.service';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonButton,  IonContent, ChartComponent, FormsModule, IonButton ]
+  imports: [IonButton, IonContent, ChartComponent, FormsModule, IonButton],
 })
 export class HomePage implements OnInit {
-
   accountId!: number;
-  total!: number;
+  total: number = 0;
   available: number = 0;
   balance: number = 0;
-  expenses: number = -250;
   isEditing: boolean = false;
 
-  constructor(private router: Router, private serviceA: AccountService) { 
-  }
+  constructor(
+    private router: Router,
+    private serviceA: AccountService,
+    private serviceD: DetailService
+  ) {}
 
   ngOnInit() {
     this.isEditing = false;
-    const storageValue = localStorage.getItem('accountId');
+    const storageAccount = localStorage.getItem('accountId');
+    const storageTotal = localStorage.getItem('total');
 
-    if (storageValue) {
-      this.accountId = parseInt(storageValue);
+    if (storageAccount) {
+      this.accountId = parseInt(storageAccount);
 
-      this.serviceA.getById(this.accountId).subscribe(data => {
+      this.serviceA.getById(this.accountId).subscribe((data) => {
         this.balance = data.balance;
-        this.available = this.balance + this.expenses;
-        console.log('home accountId' + this.accountId)
-        console.log(data)
       });
+
+      const startDate = dayjs().startOf('month').toDate();
+      const endDate = dayjs().endOf('month').toDate();
+
+      this.serviceD
+        .get(
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          startDate,
+          endDate
+        )
+        .subscribe((data) => {
+          data.map((d) => {
+            this.total += d.amount;
+        });
+        localStorage.setItem('total', this.total.toString());
+        this.available = this.balance + this.total;
+        });
+
+      if (storageTotal) {
+        this.total = parseFloat(storageTotal);
+        this.available = this.balance + this.total;
+      }
     }
   }
 
   changeIsEditing(): void {
     this.isEditing = !this.isEditing;
-    this.available = this.balance + this.expenses;
+    const userId = localStorage.getItem('userId');
+
+    if (userId) {
+      const account = this.serviceA
+        .getById(this.accountId)
+        .subscribe((data) => {
+          const form: AccountUpdateForm = {
+            id: this.accountId,
+            name: data.name,
+            balance: this.balance,
+            userId: parseInt(userId),
+          };
+          this.serviceA.update(form);
+        });
+    }
+
+    this.available = this.balance + this.total;
   }
 
   redirectToInput(): void {
-    this.router.navigate(['/input'])
+    this.router.navigate(['/input']);
   }
 }
