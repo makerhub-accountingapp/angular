2a158bd0c9a8fb30cabd8473044702e5ff15cd52
Yuki <yuki.marchal1216@gmail.com>
2025-02-14 14:51:01 +0100

    working on menu bar

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,20 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormsModule } from '@angular/forms';
 import { RouterLink, RouterModule } from '@angular/router';
-import { IonButtons, IonButton, IonHeader, IonMenuButton, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButtons, IonButton, IonHeader, IonMenuButton, IonTitle, IonToolbar, IonSelect, IonList, IonItem, IonSelectOption } from '@ionic/angular/standalone';
 import { Account } from 'src/app/core/models/account.model';
 import { User } from 'src/app/core/models/user.model';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [ IonButtons, IonHeader, IonMenuButton, IonTitle, IonToolbar, RouterModule, IonButton ],
+  imports: [ IonButtons, IonHeader, IonMenuButton, IonTitle, IonToolbar, RouterModule, IonButton, IonSelect, IonList, IonItem, IonSelectOption, FormsModule ],
 })
 export class HeaderComponent  implements OnInit {
 
   @Input({required: true}) currentUser!: User
-  accounts!: Account[];
-  currentAccount!: Account;
+  accounts: Account[] = [
+    {id: 1, name: 'Main', balance: 50, userId: 1, user: this.currentUser, transactions: []},
+    {id: 2, name: 'Sub', balance: 100, userId: 1, user: this.currentUser, transactions: []},
+
+  ];
+  currentAccountId: number = 1;
 
   constructor() {
 
